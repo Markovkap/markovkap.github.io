82e34777b9de4b886ef95930ed291b6a7a3f26d9
Markovkin <terentismark@gmail.com>
2019-06-03 20:43:51 +0300

    very hard for your mind

--- shta.js
+++ shta.js
@@ -1,167 +1,172 @@
 var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        var width = canvas.width;
-        var height = canvas.height;
-        var blockSize = 10;
-        var widthInBlocks = width / blockSize;
-        var heightInBlocks = height / blockSize;
-        var score = 0;
-        var time = 1000;
-        var gg;
-        var cl = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"];
-        var drawBorder = function () {
-            ctx.fillStyle = "Gray";
-            ctx.fillRect(0, 0, width, blockSize);
-            ctx.fillRect(0, height - blockSize, width, blockSize);
-            ctx.fillRect(0, 0, blockSize, height);
-            ctx.fillRect(width - blockSize, 0, blockSize, height);
-        };
-        var drawScore = function () {
-            ctx.font = "20px Comic Sans MS";
-            ctx.fillStyle = "Black";
-            ctx.textAlign = "left";
-            ctx.textBaseline = "top";
-            ctx.fillText("Счет: " + score, blockSize, blockSize);
-        };
-        var gameOver = function () {
-            clearInterval(intervalId);
-            ctx.font = "60px Comic Sans MS";
-            ctx.fillStyle = "Black";
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.fillText("Конец игры", width / 2, height / 2);
-        };
-        var circle = function (x, y, radius, fillCircle) {
-            ctx.beginPath();
-            ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-            if (fillCircle) {
-                ctx.fill();
-            } else {
-                ctx.stroke();
-            }
-        };
-        var Block = function (col, row) {
-            this.col = col;
-            this.row = row;
-        };
-        Block.prototype.drawSquare = function (color) {
-            var x = this.col * blockSize;
-            var y = this.row * blockSize;
-            ctx.fillStyle = cl[Math.floor(Math.random() * cl.length)];;
-            ctx.fillRect(x, y, blockSize, blockSize);
-        };
-        Block.prototype.drawCircle = function (color) {
-            var centerX = this.col * blockSize + blockSize / 2;
-            var centerY = this.row * blockSize + blockSize / 2;
-            ctx.fillStyle = color;
-            circle(centerX, centerY, blockSize / 2, true);
-        };
-        Block.prototype.equal = function (otherBlock) {
-            return this.col === otherBlock.col && this.row === otherBlock.row;
-        };
-        var Snake = function () {
-            this.segments = [
-                new Block(7, 5),
-                new Block(6, 5),
-                new Block(5, 5)
-            ];
-            this.direction = "right";
-            this.nextDirection = "right";
-        };
-        Snake.prototype.draw = function () {
-            for (var i = 0; i < this.segments.length; i++) {
-                this.segments[i].drawSquare();
-            }
-        };
-        Snake.prototype.move = function () {
-            var head = this.segments[0];
-            var newHead;
-            this.direction = this.nextDirection;
-            if (this.direction === "right") {
-                newHead = new Block(head.col + 1, head.row);
-            } else if (this.direction === "down") {
-                newHead = new Block(head.col, head.row + 1);
-            } else if (this.direction === "left") {
-                newHead = new Block(head.col - 1, head.row);
-            } else if (this.direction === "up") {
-                newHead = new Block(head.col, head.row - 1);
-            }
-            if (this.checkCollision(newHead)) {
-                gameOver();
-                return;
-            }
-            this.segments.unshift(newHead);
-            if (newHead.equal(apple.position)) {
-                score++;
-                time=time-100;
-                apple.move();
-            } else {
-                this.segments.pop();
-            }
-        };
-        Snake.prototype.checkCollision = function (head) {
-            var leftCollision = (head.col === 0);
-            var topCollision = (head.row === 0);
-            var rightCollision = (head.col === widthInBlocks - 1);
-            var bottomCollision = (head.row === heightInBlocks - 1);
-            var wallCollision = leftCollision || topCollision ||
-                rightCollision || bottomCollision;
-            var selfCollision = false;
-            for (var i = 0; i < this.segments.length; i++) {
-                if (head.equal(this.segments[i])) {
-                    selfCollision = true;
-                }
-            }
-            return wallCollision || selfCollision;
-        };
-        Snake.prototype.setDirection = function (newDirection) {
-            if (this.direction === "up" && newDirection === "down") {
-                return;
-            } else if (this.direction === "right" && newDirection === "left") {
-                return;
-            } else if (this.direction === "down" && newDirection === "up") {
-                return;
-            } else if (this.direction === "left" && newDirection === "right") {
-                return;
-            }
-            this.nextDirection = newDirection;
-        };
-        var Apple = function () {
-            this.position = new Block(10, 10);
-        };
-        Apple.prototype.draw = function () {
-            // if(gg==1 || gg==0){
-            this.position.drawCircle("LimeGreen");
-            // }
-            // if(gg==2){
-            // this.position.drawCircle("Red");
-            // }
-        };
-        Apple.prototype.move = function () {
-            var randomCol = Math.floor(Math.random() * (widthInBlocks - 2)) + 1;
-            var randomRow = Math.floor(Math.random() * (heightInBlocks - 2)) + 1;
-            // gg = Math.floor(Math.random() * 2);
-            this.position = new Block(randomCol, randomRow);
-        };
-        var snake = new Snake();
-        var apple = new Apple();
-        var intervalId = setInterval(function () {
-            ctx.clearRect(0, 0, width, height);
-            drawScore();
-            snake.move();
-            snake.draw();
-            apple.draw();
-            drawBorder();
-        }, 100);
-        var directions = {
-            37: "left",
-            38: "up",
-            39: "right",
-            40: "down"
-        };
-        $("body").keydown(function (event) {
-            var newDirection = directions[event.keyCode];
-            if (newDirection !== undefined) {
-                snake.setDirection(newDirection);
-            }
-        });
+var ctx = canvas.getContext("2d");
+var blockSize = 10;
+var width, height, gg, k;
+if (window.innerWidth > 1400) {
+    k = 0.8;
+} else {
+    k = 0.96;
+}
+width = canvas.width = Math.floor(window.innerWidth / blockSize * k) * blockSize;
+height = canvas.height = Math.floor(window.innerHeight / blockSize * k) * blockSize;
+var widthInBlocks = width / blockSize;
+var heightInBlocks = height / blockSize;
+var score = 0;
+var time = 1000;
+var cl = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"];
+var drawBorder = function () {
+    ctx.fillStyle = "Gray";
+    ctx.fillRect(0, 0, width, blockSize);
+    ctx.fillRect(0, height - blockSize, width, blockSize);
+    ctx.fillRect(0, 0, blockSize, height);
+    ctx.fillRect(width - blockSize, 0, blockSize, height);
+};
+var drawScore = function () {
+    ctx.font = "20px Comic Sans MS";
+    ctx.fillStyle = "Black";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Счет: " + score, blockSize, blockSize);
+};
+var gameOver = function () {
+    clearInterval(intervalId);
+    ctx.font = "60px Comic Sans MS";
+    ctx.fillStyle = "Black";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Конец игры", width / 2, height / 2);
+};
+var circle = function (x, y, radius, fillCircle) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    if (fillCircle) {
+        ctx.fill();
+    } else {
+        ctx.stroke();
+    }
+};
+var Block = function (col, row) {
+    this.col = col;
+    this.row = row;
+};
+Block.prototype.drawSquare = function (color) {
+    var x = this.col * blockSize;
+    var y = this.row * blockSize;
+    ctx.fillStyle = cl[Math.floor(Math.random() * cl.length)];;
+    ctx.fillRect(x, y, blockSize, blockSize);
+};
+Block.prototype.drawCircle = function (color) {
+    var centerX = this.col * blockSize + blockSize / 2;
+    var centerY = this.row * blockSize + blockSize / 2;
+    ctx.fillStyle = color;
+    circle(centerX, centerY, blockSize / 2, true);
+};
+Block.prototype.equal = function (otherBlock) {
+    return this.col === otherBlock.col && this.row === otherBlock.row;
+};
+var Snake = function () {
+    this.segments = [
+        new Block(7, 5),
+        new Block(6, 5),
+        new Block(5, 5)
+    ];
+    this.direction = "right";
+    this.nextDirection = "right";
+};
+Snake.prototype.draw = function () {
+    for (var i = 0; i < this.segments.length; i++) {
+        this.segments[i].drawSquare();
+    }
+};
+Snake.prototype.move = function () {
+    var head = this.segments[0];
+    var newHead;
+    this.direction = this.nextDirection;
+    if (this.direction === "right") {
+        newHead = new Block(head.col + 1, head.row);
+    } else if (this.direction === "down") {
+        newHead = new Block(head.col, head.row + 1);
+    } else if (this.direction === "left") {
+        newHead = new Block(head.col - 1, head.row);
+    } else if (this.direction === "up") {
+        newHead = new Block(head.col, head.row - 1);
+    }
+    if (this.checkCollision(newHead)) {
+        gameOver();
+        return;
+    }
+    this.segments.unshift(newHead);
+    if (newHead.equal(apple.position)) {
+        score++;
+        time = time - 100;
+        apple.move();
+    } else {
+        this.segments.pop();
+    }
+};
+Snake.prototype.checkCollision = function (head) {
+    var leftCollision = (head.col === 0);
+    var topCollision = (head.row === 0);
+    var rightCollision = (head.col === widthInBlocks - 1);
+    var bottomCollision = (head.row === heightInBlocks - 1);
+    var wallCollision = leftCollision || topCollision ||
+        rightCollision || bottomCollision;
+    var selfCollision = false;
+    for (var i = 0; i < this.segments.length; i++) {
+        if (head.equal(this.segments[i])) {
+            selfCollision = true;
+        }
+    }
+    return wallCollision || selfCollision;
+};
+Snake.prototype.setDirection = function (newDirection) {
+    if (this.direction === "up" && newDirection === "down") {
+        return;
+    } else if (this.direction === "right" && newDirection === "left") {
+        return;
+    } else if (this.direction === "down" && newDirection === "up") {
+        return;
+    } else if (this.direction === "left" && newDirection === "right") {
+        return;
+    }
+    this.nextDirection = newDirection;
+};
+var Apple = function () {
+    this.position = new Block(10, 10);
+};
+Apple.prototype.draw = function () {
+    // if(gg==1 || gg==0){
+    this.position.drawCircle("LimeGreen");
+    // }
+    // if(gg==2){
+    // this.position.drawCircle("Red");
+    // }
+};
+Apple.prototype.move = function () {
+    var randomCol = Math.floor(Math.random() * (widthInBlocks - 2)) + 1;
+    var randomRow = Math.floor(Math.random() * (heightInBlocks - 2)) + 1;
+    // gg = Math.floor(Math.random() * 2);
+    this.position = new Block(randomCol, randomRow);
+};
+var snake = new Snake();
+var apple = new Apple();
+var intervalId = setInterval(function () {
+    ctx.clearRect(0, 0, width, height);
+    drawScore();
+    snake.move();
+    snake.draw();
+    apple.draw();
+    drawBorder();
+}, 100);
+var directions = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+};
+$("body").keydown(function (event) {
+    var newDirection = directions[event.keyCode];
+    if (newDirection !== undefined) {
+        snake.setDirection(newDirection);
+    }
+});
